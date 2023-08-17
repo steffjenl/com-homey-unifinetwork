6b2e28f5b30e32d81c70d2fa18e394f249e197ea
Stephan Eizinga <stephan@monkeysoft.nl>
2023-08-17 21:38:55 +0200

    Bugfix: alarm_connected capabilities Feature: test connection credentials Feature: more information in settings page

--- api.js
+++ api.js
@@ -1,6 +1,35 @@
+const ApiClient = require("./library/apiclient");
 module.exports = {
     async getSites({homey, query}) {
         const result = await homey.app.api.unifi.getSites();
         return result;
+    },
+
+    async testCredentials({homey, body}) {
+
+        try {
+            this.api = new ApiClient({homey: this.homey});
+            const console = this.api.setUnifiObject(body.host, body.port, body.user, body.pass, body.site);
+            const loggedIn = await this.api.unifi.login(body.user, body.pass);
+            const accessPoints = await this.api.unifi.getAccessDevices();
+            const wifiDevices = await this.api.unifi.getClientDevices();
+            const allUsers = await this.api.unifi.getAllUsers();
+            const loggedOut = await this.api.unifi.logout();
+
+            return {
+                status: 'success',
+            };
+        } catch (error) {
+            console.log('testCredentials error', error);
+            return {
+                status: 'failure',
+                error: error.message,
+            };
+        }
+
+
+        return {
+            status: 'failure',
+        };
     }
 };
