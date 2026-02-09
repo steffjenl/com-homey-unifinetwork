be2c4bc75ea273deb2755dad91af89f0d1bac9f2
Stephan Eizinga <stephan@monkeysoft.nl>
2026-02-09 21:43:11 +0100

    Add access-point

---
+++ drivers/access-point/device.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const {Device} = require('homey');
+
+class AccessPointDevice extends Device {
+
+    /**
+     * onInit is called when the device is initialized.
+     */
+    async onInit() {
+        await this.waitForBootstrap();
+        this.log('Access-Point has been initialized');
+    }
+
+    async waitForBootstrap() {
+        if (this.homey.app.loggedIn === true) {
+            await this._createMissingCapabilities();
+            await this.getDeviceStatus();
+        } else {
+            this.homey.setTimeout(this.waitForBootstrap.bind(this), 250);
+        }
+    }
+
+    /**
+     * onAdded is called when the user adds the device, called just after pairing.
+     */
+    async onAdded() {
+        this.log('Access-Point has been added');
+    }
+
+    /**
+     * onSettings is called when the user updates the device's settings.
+     * @param {object} event the onSettings event data
+     * @param {object} event.oldSettings The old settings object
+     * @param {object} event.newSettings The new settings object
+     * @param {string[]} event.changedKeys An array of keys changed since the previous version
+     * @returns {Promise<string|void>} return a custom message that will be displayed
+     */
+    async onSettings({oldSettings, newSettings, changedKeys}) {
+        this.log('Access-Point settings where changed');
+    }
+
+    /**
+     * onRenamed is called when the user updates the device's name.
+     * This method can be used this to synchronise the name to the device.
+     * @param {string} name The new name
+     */
+    async onRenamed(name) {
+        this.log('Access-Point was renamed');
+    }
+
+    /**
+     * onDeleted is called when the user deleted the device.
+     */
+    async onDeleted() {
+        this.log('Access-Point has been deleted');
+    }
+
+    async _createMissingCapabilities() {
+        if (this.getClass() !== 'sensor') {
+            this.homey.app.debug(`changed class to sensor for ${this.getName()}`);
+            this.setClass('sensor').catch(this.error);
+        }
+    }
+
+    getDeviceStatus() {
+        if (this.homey.app.loggedIn === true) {
+            this.homey.app.api.unifi.getAccessDevices(this.getData().id).then(device => {
+                device = device.filter(obj => obj.adopted === true);
+                device = device.filter(obj => obj.type === 'uap');
+                device = device.filter(obj => obj.mac === this.getData().id);
+                this.homey.log(JSON.stringify(device));
+
+            }).catch(error => this.homey.app.debug(error));
+        }
+    }
+
+    onUpdateMessagePayload(playloadMessage) {
+        if (typeof playloadMessage.ip !== 'undefined') {
+            this.onIPChange(playloadMessage);
+        }
+
+    }
+}
+
+module.exports = AccessPointDevice;
---
+++ drivers/access-point/driver.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const {Driver} = require('homey');
+const UnifiConstants = require("../../library/constants");
+const Homey = require("homey");
+
+class AccessPoint extends Driver {
+
+    /**
+     * onInit is called when the driver is initialized.
+     */
+    async onInit() {
+        this.log('Access-Point has been initialized');
+    }
+
+    /**
+     * onPairListDevices is called when a user is adding a device
+     * and the 'list_devices' view is called.
+     * This should return an array with the data of devices that are available for pairing.
+     */
+    async onPairListDevices() {
+        return Object.values(await this.homey.app.api.getAccessPoints()).map(device => {
+            let deviceName = this.homey.app.api.getDeviceName(device);
+
+            return {
+                data: {id: String(device.mac)},
+                name: deviceName,
+            };
+        });
+    }
+
+    getUnifiDeviceById(deviceId) {
+        try {
+            const devices = this.getDevices();
+            const device = devices.find(device => String(device.getData().id) === String(deviceId));
+            if (!device) return false;
+            return device;
+        } catch (Error) {
+            return false;
+        }
+    }
+}
+
+module.exports = AccessPoint;
